febd7b6d80351e5ee76099227651b362fcd060a9
jimmyochoa <jimmyochoaholguin@gmail.com>
2025-07-03 19:12:16 -0500

    initial commit

---
+++ app/seeders/20250703235609-fotos.js
@@ -0,0 +1,26 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    for (let i = 0; i < 10; i++) {
+      await queryInterface.bulkInsert('fotos', [{
+        titulo: 'fotos' + i,
+        descripcion: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+        calificacion: (Math.random() * 10).toFixed(2),
+        ruta: 'public/images/' + 'fotos' + i + '.png',
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }], {});
+    }
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
